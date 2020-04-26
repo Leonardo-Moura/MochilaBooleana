834a83d72d2b31cccb1633b0212e69c5c4f4e3bc
Leonardo-Moura <leonardooliveiram@hotmail.com>
2020-04-26 02:05:02 -0300

    Adicionado o cogido de programacao dinamica para uma maquina.

--- MochilaBooleana_ProgramacaoDinamica.c
+++ MochilaBooleana_ProgramacaoDinamica.c
@@ -6,23 +6,24 @@
             Aluno: Leonardo Oliveira de Moura
             Aluno: Matheus Inhesta
 
-Resolve o problema da mochila booleana de forma iterativa usando forca bruta.
-Nao utiliza programacao dinamica.
-Utiliza a abordagem de Job Farm para distribuição das tarefas entre
-as unidades de processamento.
+Resolve o problema da mochila booleana de forma iterativa usando 
+programacao dinamica.
+Nao utiliza processamento paralelo.
+Usado para comaparar o tempo de execucao de um codigo por forca bruta com
+processamento paralelo com um executado em uma maquina com programacao
+dinamica.
 
 --------------------------------------------------------------------*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#include "mpi.h"
+#include <limits.h>
 
 #define ERRO -1
 #define FALSE 0
 #define TRUE 1
 #define TAM_LINHA 10
-#define TAG 2587
 
 typedef struct
 {
@@ -31,126 +32,74 @@ typedef struct
     int valor;
 } objeto;
 
-int EncontraValorMaximoDeFormaDistribuida(int capacidade, int qtdeObjetos, objeto *objetos);
-int SincronizaValorMaximoEntreUnidadesDeProcessamento(int valorMaximoAtual);
-int RecebeValoresMaximosDeMaquinasSlave();
 int EncontraValorMaximo(int, int, objeto*);
 int LeArquivo(int*, int*, objeto**);
+int InicializaMemoria(int, int);
+int LeValorDaMemoria(int indiceObjeto, int capacidade);
+void EscreveValorNaMemoria(int indiceObjeto, int capacidade, int valor);
 int AbreArquivo(FILE **);
-void PrintaInformacoesDoArquivo(int capacidade, int qtdeObjetos);
-void PrintaCabecalhoErro();
-void PrintaObjetos(int, objeto*);
+void printaCabecalhoErro();
+void printaObjetos(int, objeto*);
 int LeInteiro(FILE*);
 objeto* LeObjetos(int, FILE*);
 objeto TransformarLinhaEmObjeto(char*, int);
 char **SplitString(char*);
 
-//Funcoes de acesso a informacoes da biblioteca MPI
-int GetIdDaUnidadeDeProcessamento(); //rank
-int GetNumeroDeUnidadesDeProcessamento(); //size
+int **memoria;
 
-int main(int argc, char *argv[])
+int main()
 {
-    int valorMaximo;
+    int loop, valorMaximo;
     int capacidade, qtdeObjetos;
     clock_t inicio, fim;
     objeto *objetos;
 
-    MPI_Init(&argc, &argv);
-
     if (!LeArquivo(&capacidade, &qtdeObjetos, &objetos))
         return 1;
 
-    PrintaInformacoesDoArquivo(capacidade, qtdeObjetos);
-    
+    if (!InicializaMemoria(capacidade, qtdeObjetos))
+        return 1;
+
+    printf("\n\nRealizando a busca de combinacoes.");
     inicio = clock();
-    valorMaximo = EncontraValorMaximoDeFormaDistribuida(capacidade, qtdeObjetos, objetos);
-    valorMaximo = SincronizaValorMaximoEntreUnidadesDeProcessamento(valorMaximo);
+    valorMaximo = EncontraValorMaximo(capacidade, qtdeObjetos, objetos);
     fim = clock();
 
-	if(GetIdDaUnidadeDeProcessamento() == 0){
-	    printf("\nValor maximo suportado: %d", valorMaximo);
-	    printf("\nTempo gasto em milissegundos: %g", ((double)(fim - inicio)) * 1000.0 / CLOCKS_PER_SEC);
-	    printf("\n\n");
-	}
-
-	MPI_Finalize();
-}
-
-int EncontraValorMaximoDeFormaDistribuida(int capacidade, int qtdeObjetos, objeto *objetos)
-{
-    int loop, valorMaximo, valorDoLoop, capacidadeDoLoop;
-
-    valorMaximo = 0;
-    valorDoLoop = 0;
-    capacidadeDoLoop = 0;
-
-    for(loop = GetIdDaUnidadeDeProcessamento(); loop < qtdeObjetos; loop += getNumeroDeUnidadesDeProcessamento())
-    {
-        if (objetos[loop].peso > capacidade)
-            continue;
-
-        valorDoLoop = objetos[loop].valor;
-        capacidadeDoLoop = capacidade - objetos[loop].peso;
-
-        valorDoLoop += EncontraValorMaximo(capacidadeDoLoop, qtdeObjetos, objetos);
-
-        if (valorDoLoop > valorMaximo)
-            valorMaximo = valorDoLoop;
-    }
-
-    return valorMaximo;
-}
-
-int SincronizaValorMaximoEntreUnidadesDeProcessamento(int valorMaximoAtual)
-{
-    int valorMaximoDeOutrasMaquinas = 0;
-    if (GetIdDaUnidadeDeProcessamento() == 0)
-    {
-        valorMaximoDeOutrasMaquinas = RecebeValoresMaximosDeMaquinasSlave();
-    }
-    else
-    {
-        MPI_Send(&valorMaximoAtual, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
-    }
-    
-    if (valorMaximoDeOutrasMaquinas > valorMaximoAtual)
-        valorMaximoAtual = valorMaximoDeOutrasMaquinas;
-
-    return valorMaximoAtual;
-}
-
-int RecebeValoresMaximosDeMaquinasSlave()
-{
-    int valorMaximo = 0, valorDoLoop = 0, loop;
-
-    for (loop = 1; loop < getNumeroDeUnidadesDeProcessamento(); loop++)
-    {
-        MPI_Recv(&valorDoLoop, 1, MPI_INT, loop, TAG, MPI_COMM_WORLD, NULL);
-
-        if (valorDoLoop > valorMaximo)
-            valorMaximo = valorDoLoop;
-    }
-
-    return valorMaximo;
+    printf("\nValor maximo suportado: %d", valorMaximo);
+    printf("\nTempo gasto em milissegundos: %g", ((double)(fim - inicio)) * 1000.0 / CLOCKS_PER_SEC);
+    printf("\n\n");
 }
 
 int EncontraValorMaximo(int capacidade, int qtdeObjetos, objeto *objetos)
 {
     int loop, valorMaximo, valorDoLoop, capacidadeDoLoop;
 
+    if (capacidade == 0) return 0;
+
     valorMaximo = 0;
     valorDoLoop = 0;
     capacidadeDoLoop = 0;
     for (loop = 0; loop < qtdeObjetos; loop++)
     {
-        if (objetos[loop].peso > capacidade)
-            continue;
+        if (LeValorDaMemoria(loop, capacidade) == -1)
+        {
+            if (objetos[loop].peso > capacidade)
+            {
+                EscreveValorNaMemoria(loop, capacidade, INT_MIN);
+                continue;
+            }
 
-        valorDoLoop = objetos[loop].valor;
-        capacidadeDoLoop = capacidade - objetos[loop].peso;
+            valorDoLoop = objetos[loop].valor;
+            capacidadeDoLoop = capacidade - objetos[loop].peso;
 
-        valorDoLoop += EncontraValorMaximo(capacidadeDoLoop, qtdeObjetos, objetos);
+            valorDoLoop += EncontraValorMaximo(capacidadeDoLoop, qtdeObjetos, objetos);
+            EscreveValorNaMemoria(loop, capacidade, valorDoLoop);
+        }
+        else
+        {
+            valorDoLoop = LeValorDaMemoria(loop, capacidade);
+        }
+        
 
         if (valorDoLoop > valorMaximo)
             valorMaximo = valorDoLoop;
@@ -169,9 +118,11 @@ int LeArquivo(int *capacidade, int *qtdeObjetos, objeto **objetos)
 
     if ((*capacidade = LeInteiro(dados)) == ERRO)
         return FALSE;
+    printf("\nCapacidade: %d", *capacidade);
 
     if ((*qtdeObjetos = LeInteiro(dados)) == ERRO)
         return FALSE;
+    printf("\nQuantidade de objetos: %d", *qtdeObjetos);
     
     if ((*objetos = LeObjetos(*qtdeObjetos, dados)) == NULL)
         return FALSE;
@@ -181,19 +132,56 @@ int LeArquivo(int *capacidade, int *qtdeObjetos, objeto **objetos)
     return TRUE;
 }
 
+int InicializaMemoria(int capacidade, int qtdeObjetos)
+{
+    int loopLinhas, loopColunas;
+    int tamanhoMatriz = (sizeof(int) * capacidade) * (sizeof(int) * qtdeObjetos);
+    memoria = (int **) malloc(sizeof(int) * qtdeObjetos);
+    int cont = 0;
+
+    if (memoria == NULL)
+    {
+        printaCabecalhoErro();
+        printf("Não foi possível resevar a memória necessária.");
+        return FALSE;
+    }
+
+    for (loopLinhas = 0; loopLinhas < qtdeObjetos; loopLinhas++)
+    {
+        memoria[loopLinhas] = (int *) malloc(sizeof(int) * capacidade);
+        for (loopColunas = 0; loopColunas < capacidade; loopColunas++)
+        {
+            memoria[loopLinhas][loopColunas] = -1;
+        }
+    }
+
+    return TRUE;
+}
+
+int LeValorDaMemoria(int indiceObjeto, int capacidade)
+{
+    return memoria[indiceObjeto][capacidade - 1];
+}
+
+void EscreveValorNaMemoria(int indiceObjeto, int capacidade, int valor)
+{
+    //printf("\nEscrevendo objeto %d, capacidade %d = %d", indiceObjeto, capacidade - 1, valor);
+    memoria[indiceObjeto][capacidade - 1] = valor;
+}
+
 int AbreArquivo(FILE **dados)
 {
     FILE *arquivo;
     if ((arquivo = fopen("dados.txt", "r")) == NULL)
     {
-        PrintaCabecalhoErro();
+        printaCabecalhoErro();
         printf("Nao foi possivel abrir o arquivo 'dados.txt'.");
         return FALSE;
     }
 
     if(feof(arquivo))
     {
-        PrintaCabecalhoErro();
+        printaCabecalhoErro();
         printf("Arquivo 'dados.txt' vazio.");
         return FALSE;
     }
@@ -202,22 +190,12 @@ int AbreArquivo(FILE **dados)
     return TRUE;
 }
 
-void PrintaInformacoesDoArquivo(int capacidade, int qtdeObjetos)
-{
-    if (GetIdDaUnidadeDeProcessamento() == 0)
-    {
-        printf("\n");
-        printf("Capacidade da mochila: %d", capacidade);
-        printf("\nQuantidade de objetos: %d", qtdeObjetos);
-    }
-}
-
-void PrintaCabecalhoErro()
+void printaCabecalhoErro()
 {
     printf("\n\n---------- ERRO ----------\n");
 }
 
-void PrintaObjetos(int qtdeObjetos, objeto *objetos)
+void printaObjetos(int qtdeObjetos, objeto *objetos)
 {
     int loop;
     for (loop = 0; loop < qtdeObjetos; loop++)
@@ -236,7 +214,7 @@ int LeInteiro(FILE *dados)
     memset(linha, 0, sizeof(char) * TAM_LINHA);
     if (fgets(linha, TAM_LINHA, dados) == NULL)
     {
-        PrintaCabecalhoErro();
+        printaCabecalhoErro();
         printf("Erro ao carregar valor do arquivo.");
         return ERRO;
     }
@@ -255,7 +233,7 @@ objeto* LeObjetos(int qtdeObjetos, FILE *dados)
     {
         if (loop >= qtdeObjetos)
         {
-            PrintaCabecalhoErro();
+            printaCabecalhoErro();
             printf("Existem mais objetos do que o informado no totalizador.");
             free(objetos);
             return NULL;
@@ -265,7 +243,7 @@ objeto* LeObjetos(int qtdeObjetos, FILE *dados)
 
         if (objetos[loop].peso == 0 || objetos[loop].valor == 0)
         {
-            PrintaCabecalhoErro();
+            printaCabecalhoErro();
             printf("Erro ao ler objeto na linha %d", loop + 3);
             free(objetos);
             return NULL;
@@ -323,24 +301,4 @@ char **SplitString(char stringInteira[])
     retorno[1][loop] = '\0';
 
     return retorno;
-}
-
-int GetIdDaUnidadeDeProcessamento()
-{
-    static int id = -1;
-
-    if (id == -1)
-        MPI_Comm_rank(MPI_COMM_WORLD, &id);
-
-    return id;
-}
-
-int getNumeroDeUnidadesDeProcessamento()
-{
-    static int size = -1;
-
-    if (size == -1)
-        MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    return size;
 }
